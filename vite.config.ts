06ae9f6c9c27431d6e55d2a888f621a62df9c41e
Inderkumar kulkarni <inderkumarkulkarni@gmail.com>
2026-07-08 15:50:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-1-/',  // ← Your GitHub repo name
+  base: '/portfolio-1-/', // <-- ADD THIS LINE EXACTLY
 })
